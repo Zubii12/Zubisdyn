ebe8110ff180e8f150ff4630582f13fdf6d5de70
Zubii <ionutzubascu12@gmail.com>
2021-05-07 23:53:58 +0300

    * config dev team * small changes in some pages

--- mobile/lib/src/presentation/auth/forgot_password/code_verification_page.dart
+++ mobile/lib/src/presentation/auth/forgot_password/code_verification_page.dart
@@ -30,145 +30,148 @@ class _CodeVerificationPageState extends State<CodeVerificationPage> with StoreM
           ),
         ),
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              const SizedBox(height: 40),
-              const Center(
-                child: FlutterLogo(
-                  size: 42.0,
-                ),
-              ),
-              const SizedBox(height: 40),
-              Container(
-                width: size.width < maxWidth ? size.width - 24.0 : maxWidth,
-                child: const Text(
-                  'Enter the 4 digits code that you received on'
-                  ' your email so you can continue to reset your'
-                  ' account password.',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                    color: Color(0xFF616161),
-                    height: 1.5,
+          child: SingleChildScrollView(
+            reverse: true,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const SizedBox(height: 40),
+                const Center(
+                  child: FlutterLogo(
+                    size: 42.0,
                   ),
                 ),
-              ),
-              Container(
-                constraints: const BoxConstraints.expand(height: 120.0),
-                margin: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 40.0),
-                child: Stack(
-                  children: <Widget>[
-                    Opacity(
-                      opacity: 0.0,
-                      child: TextField(
-                        controller: _code,
-                        focusNode: _node,
-                        autofocus: true,
-                        autofillHints: const <String>[AutofillHints.oneTimeCode],
-                        keyboardType: TextInputType.number,
-                        onChanged: (String code) {
-                          // dispatch(UpdateRegistrationInfo(smsCode: code));
-                          setState(() {});
-                          // if (code.length == _kSmsCodeLength && !isLoading) {
-                          //   if (type == AuthType.changePhone) {
-                          //     dispatch(ChangePhone.start(result: _onResult));
-                          //   } else {
-                          //     dispatch(LoginWithSmsCode.start(result: _onResult));
-                          //   }
-                          // }
-                        },
-                        inputFormatters: <TextInputFormatter>[
-                          FilteringTextInputFormatter.digitsOnly,
-                          LengthLimitingTextInputFormatter(6),
-                        ],
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        FocusScope.of(context).requestFocus(_node);
-                      },
-                      child: FormField<String>(
-                        initialValue: _code.text,
-                        builder: (FormFieldState<String> state) {
-                          return InputDecorator(
-                            decoration: InputDecoration(
-                              filled: false,
-                              contentPadding: EdgeInsets.zero,
-                              errorText: state.errorText,
-                              border: InputBorder.none,
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: List<Widget>.generate(
-                                _kSmsCodeLength,
-                                (int index) {
-                                  final bool isCurrent = index == _code.text.length;
-
-                                  String digit;
-                                  if (_code.text.length - 1 < index) {
-                                    digit = '';
-                                  } else {
-                                    digit = _code.text[index];
-                                  }
-
-                                  return Container(
-                                    width: 50.0,
-                                    height: 72.0,
-                                    margin: EdgeInsetsDirectional.only(end: index == _kSmsCodeLength - 1 ? 0 : 8.0),
-                                    child: InputDecorator(
-                                      isEmpty: digit.isEmpty,
-                                      expands: false,
-                                      isFocused: isCurrent,
-                                      decoration: InputDecoration(
-                                        border: OutlineInputBorder(
-                                          borderRadius: BorderRadius.circular(8.0),
-                                        ),
-                                      ),
-                                      child: Text(
-                                        digit,
-                                        textAlign: TextAlign.center,
-                                        style: const TextStyle(
-                                          fontSize: 30.0,
-                                        ),
-                                      ),
-                                    ),
-                                  );
-                                },
-                              ),
-                            ),
-                          );
-                        },
-                        validator: (String code) {
-                          if (code.length != _kSmsCodeLength) {
-                            return 'The code is 6 digits long.';
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 28),
-                width: size.width < maxWidth ? size.width - 24.0 : maxWidth,
-                child: TextButton(
-                  onPressed: () {
-                    // todo
-                    //Navigator.pushNamed(context, AppRoutes.codeVerification);
-                  },
+                const SizedBox(height: 40),
+                Container(
+                  width: size.width < maxWidth ? size.width - 24.0 : maxWidth,
                   child: const Text(
-                    'Continue',
+                    'Enter the 4 digits code that you received on'
+                    ' your email so you can continue to reset your'
+                    ' account password.',
                     style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF616161),
+                      height: 1.5,
                     ),
                   ),
                 ),
-              ),
-            ],
+                Container(
+                  constraints: const BoxConstraints.expand(height: 120.0),
+                  margin: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 40.0),
+                  child: Stack(
+                    children: <Widget>[
+                      Opacity(
+                        opacity: 0.0,
+                        child: TextField(
+                          controller: _code,
+                          focusNode: _node,
+                          autofocus: true,
+                          autofillHints: const <String>[AutofillHints.oneTimeCode],
+                          keyboardType: TextInputType.number,
+                          onChanged: (String code) {
+                            // dispatch(UpdateRegistrationInfo(smsCode: code));
+                            setState(() {});
+                            // if (code.length == _kSmsCodeLength && !isLoading) {
+                            //   if (type == AuthType.changePhone) {
+                            //     dispatch(ChangePhone.start(result: _onResult));
+                            //   } else {
+                            //     dispatch(LoginWithSmsCode.start(result: _onResult));
+                            //   }
+                            // }
+                          },
+                          inputFormatters: <TextInputFormatter>[
+                            FilteringTextInputFormatter.digitsOnly,
+                            LengthLimitingTextInputFormatter(6),
+                          ],
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          FocusScope.of(context).requestFocus(_node);
+                        },
+                        child: FormField<String>(
+                          initialValue: _code.text,
+                          builder: (FormFieldState<String> state) {
+                            return InputDecorator(
+                              decoration: InputDecoration(
+                                filled: false,
+                                contentPadding: EdgeInsets.zero,
+                                errorText: state.errorText,
+                                border: InputBorder.none,
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: List<Widget>.generate(
+                                  _kSmsCodeLength,
+                                  (int index) {
+                                    final bool isCurrent = index == _code.text.length;
+
+                                    String digit;
+                                    if (_code.text.length - 1 < index) {
+                                      digit = '';
+                                    } else {
+                                      digit = _code.text[index];
+                                    }
+
+                                    return Container(
+                                      width: 50.0,
+                                      height: 72.0,
+                                      margin: EdgeInsetsDirectional.only(end: index == _kSmsCodeLength - 1 ? 0 : 8.0),
+                                      child: InputDecorator(
+                                        isEmpty: digit.isEmpty,
+                                        expands: false,
+                                        isFocused: isCurrent,
+                                        decoration: InputDecoration(
+                                          border: OutlineInputBorder(
+                                            borderRadius: BorderRadius.circular(8.0),
+                                          ),
+                                        ),
+                                        child: Text(
+                                          digit,
+                                          textAlign: TextAlign.center,
+                                          style: const TextStyle(
+                                            fontSize: 30.0,
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            );
+                          },
+                          validator: (String code) {
+                            if (code.length != _kSmsCodeLength) {
+                              return 'The code is 6 digits long.';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 28),
+                  width: size.width < maxWidth ? size.width - 24.0 : maxWidth,
+                  child: TextButton(
+                    onPressed: () {
+                      // todo
+                      //Navigator.pushNamed(context, AppRoutes.codeVerification);
+                    },
+                    child: const Text(
+                      'Continue',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- mobile/lib/src/presentation/auth/forgot_password/forgot_password_page.dart
+++ mobile/lib/src/presentation/auth/forgot_password/forgot_password_page.dart
@@ -19,83 +19,86 @@ class ForgotPasswordPage extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 40),
-            const Center(
-              child: FlutterLogo(
-                size: 42.0,
-              ),
-            ),
-            const SizedBox(height: 40),
-            Container(
-              width: size.width < maxWidth ? size.width - 24.0 : maxWidth,
-              child: const Text(
-                'Enter your email for the verification proccess,'
-                ' and we will send 4 digits code to your email'
-                ' for the verification.',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                  color: Color(0xFF616161),
-                  height: 1.5,
+        child: SingleChildScrollView(
+          reverse: true,
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 40),
+              const Center(
+                child: FlutterLogo(
+                  size: 42.0,
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 48),
-              width: size.width < maxWidth ? size.width - 24.0 : maxWidth,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  const Padding(
-                    padding: EdgeInsets.only(bottom: 16.0),
-                    child: Text(
-                      'Email',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w500,
+              const SizedBox(height: 40),
+              Container(
+                width: size.width < maxWidth ? size.width - 24.0 : maxWidth,
+                child: const Text(
+                  'Enter your email for the verification proccess,'
+                  ' and we will send 4 digits code to your email'
+                  ' for the verification.',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF616161),
+                    height: 1.5,
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 48),
+                width: size.width < maxWidth ? size.width - 24.0 : maxWidth,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.only(bottom: 16.0),
+                      child: Text(
+                        'Email',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ),
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: 'Enter your email',
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your email',
+                      ),
+                      validator: (String value) {
+                        if (value.trim().isEmpty) {
+                          return 'Please enter an email';
+                        } else if (value.trim().contains('@') && !EmailValidator.validate(value.trim())) {
+                          return 'Please use a valid email';
+                        }
+                        return null;
+                      },
+                      onChanged: (String value) {
+                        // todo
+                      },
                     ),
-                    validator: (String value) {
-                      if (value.trim().isEmpty) {
-                        return 'Please enter an email';
-                      } else if (value.trim().contains('@') && !EmailValidator.validate(value.trim())) {
-                        return 'Please use a valid email';
-                      }
-                      return null;
-                    },
-                    onChanged: (String value) {
-                      // todo
-                    },
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 36),
-              width: size.width < maxWidth ? size.width - 24.0 : maxWidth,
-              child: TextButton(
-                onPressed: () {
-                  // todo
-                  Navigator.pushNamed(context, AppRoutes.codeVerification);
-                },
-                child: const Text(
-                  'Continue',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 36),
+                width: size.width < maxWidth ? size.width - 24.0 : maxWidth,
+                child: TextButton(
+                  onPressed: () {
+                    // todo
+                    Navigator.pushNamed(context, AppRoutes.codeVerification);
+                  },
+                  child: const Text(
+                    'Continue',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
